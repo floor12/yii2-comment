d566831f15f5532305b2744c1ca2ed4469eb13eb
floor12 <primelifecoder@gmail.com>
2017-11-23 12:44:50 +0100

    Files added to comments

--- views/_comment.php
+++ views/_comment.php
@@ -31,11 +31,12 @@ use rmrevin\yii\fontawesome\FontAwesome;
             </div>
         </div>
 
-    <div class="comment-control">
-        <?= Html::a(FontAwesome::icon('reply') . "Ответить", null, ['onclick' => "showForm('comment/form',{parent: {$model->id}})", 'class' => 'btn btn-xs btn-default']) ?>
-        <?php if ($model->canUpdate(Yii::$app->user->id) || !Yii::$app->user->can('admin')): ?>
-            <?= Html::a(FontAwesome::icon('pencil') . "Редактировать", null, ['onclick' => "showForm('comment/form',{$model->id})", 'class' => 'btn btn-xs btn-default']) ?>
-            <?= Html::a(FontAwesome::icon('trash') . "Удалить", null, ['onclick' => "deleteComment({$model->id})", 'class' => 'btn btn-xs btn-default']) ?>
-        <?php endif; ?>
+        <div class="comment-control">
+            <?= Html::a(FontAwesome::icon('reply') . "Ответить", null, ['onclick' => "showForm('comment/form',{parent: {$model->id}})", 'class' => 'btn btn-xs btn-default']) ?>
+            <?php if ($model->canUpdate(Yii::$app->user->id) || !Yii::$app->user->can('admin')): ?>
+                <?= Html::a(FontAwesome::icon('pencil') . "Редактировать", null, ['onclick' => "showForm('comment/form',{$model->id})", 'class' => 'btn btn-xs btn-default']) ?>
+                <?= Html::a(FontAwesome::icon('trash') . "Удалить", null, ['onclick' => "deleteComment({$model->id})", 'class' => 'btn btn-xs btn-default']) ?>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
